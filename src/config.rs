2b1d0129efa3d11f00a05caa5ba7f98ac02a0d11
nguquen <thienlduc@gmail.com>
2022-08-12 12:38:29 +0700

    Add more config fields

--- src/config.rs
+++ src/config.rs
@@ -10,6 +10,13 @@ pub struct StreamerConfig {
 
     pub topics: Vec<String>,
 
+    pub group_id: String,
+
+    pub auto_offset_reset: String,
+
+    #[builder(default = "6000")]
+    pub session_timeout: usize,
+
     #[builder(default = "32")]
     pub preload_pool_size: usize,
 }
@@ -20,6 +27,12 @@ impl StreamerConfig {
         self.kafka_config.iter().for_each(|(k, v)| {
             kafka_conf.set(k, v);
         });
+
+        kafka_conf.set("enable.auto.commit", "false");
+        kafka_conf.set("enable.partition.eof", "false");
+        kafka_conf.set("session.timeout.ms", self.session_timeout.to_string());
+        kafka_conf.set("group.id", self.group_id.to_owned());
+        kafka_conf.set("auto.offset.reset", self.auto_offset_reset.to_owned());
 
         kafka_conf
     }
